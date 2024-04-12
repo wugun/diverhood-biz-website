5d1ce910421b9c2a63ca58915ac85f9f9346ba1d
Aiden Liu <wugunawki@gmail.com>
2024-04-13 00:26:22 +0800

    update core feature

--- src/components/CoreFeatures.js
+++ src/components/CoreFeatures.js
@@ -21,7 +21,7 @@ const CoreFeatures = () => {
       <div className="flex-1 relative w-full">
         <div className="md:before:w-full md:before:h-full md:before:absolute md:before:-bottom-24 lg:before:-bottom-20 xl:before:-right-5 lg:before:-right-12 md:before:-right-16 md:before:bg-right-bottom md:before:bg-contain md:before:bg-no-repeat before:-z-50 before:bg-none md:before:bg-[url('/videobg.png')]">
           <Image
-            src={"/features/core-features.png"}
+            src={"/features/core-feature-1.png"}
             width={300}
             height={300}
             alt="core features"
